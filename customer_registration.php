a98419bdc267b0357a0f0fbbd0155dd7764e665c
OnuRag <34398834+OnuRag@users.noreply.github.com>
2017-12-28 20:32:55 +0600

    Update customer_registration.php

--- customer_registration.php
+++ customer_registration.php
@@ -6,7 +6,13 @@ if (isset($_GET["register"])) {
 <!DOCTYPE html>
 <html>
 	<head>
-		
+		<meta charset="UTF-8">
+		<title>e-commerce Store</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 <body>
 <div class="wait overlay">
