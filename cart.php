b36fc736903305e1f8bf96d2f40aa9c5299141f6
OnuRag <34398834+OnuRag@users.noreply.github.com>
2017-12-28 20:36:38 +0600

    Update cart.php

--- cart.php
+++ cart.php
@@ -4,14 +4,7 @@
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>e-commerce Store</title>
-		<link rel="stylesheet" href="css/bootstrap.min.css"/>
-		<script src="js/jquery2.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="main.js"></script>
-		<link rel="stylesheet" type="text/css" href="style.css"/>
+	<head>tyle.css"/>
 	</head>
 <body>
 <div class="wait overlay">
