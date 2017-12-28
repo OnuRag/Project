236a0241fa543322febcf4928c0156f9c6991b4f
OnuRag <34398834+OnuRag@users.noreply.github.com>
2017-12-28 20:34:42 +0600

    Update customer_order.php

--- customer_order.php
+++ customer_order.php
@@ -8,11 +8,7 @@ if(!isset($_SESSION["uid"])){
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="UTF-8">
-		<title>e-commerce Store</title>
-		<link rel="stylesheet" href="css/bootstrap.min.css"/>
-		<script src="js/jquery2.js"></script>
-		<script src="js/bootstrap.min.js"></script>
+		<min.js"></script>
 		<script src="main.js"></script>
 		<style>
 			table tr td {padding:10px;}
